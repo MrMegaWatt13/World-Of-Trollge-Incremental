649feaff2267de43d8fae3111c1eeb9b00dda508
MrMegaWatt13 <112874353+MrMegaWatt13@users.noreply.github.com>
2022-09-24 21:33:17 +0800

    Alpha 1.0.0

---
+++ script.js
@@ -0,0 +1,41 @@
+//Resources-------------------------------------------------------------------
+
+var OilCups = 0;
+    function gainOilCup(number){
+    OilCups = OilCups + number;
+    document.getElementById("OilCups").innerHTML = OilCups;
+}; 
+
+var BloodCups = 0;
+   function gainBloodCup(number){
+    BloodCups = BloodCups + number;
+    document.getElementById("BloodCups").innerHTML = BloodCups;
+}; 
+ 
+var MysteriousCups = 0;
+    function gainMysteriousCup(number){
+    MysteriousCups = MysteriousCups + number;
+    document.getElementById("MysteriousCups").innerHTML = MysteriousCups;
+}; 
+ 
+var SorrowCups = 0;
+    function gainSorrowCup(number){
+    SorrowCups = SorrowCups + number;
+    document.getElementById("SorrowCups").innerHTML = SorrowCups;
+}; 
+
+var NightEssence = 0;
+    function gainNightEssence(number){
+    NightEssence = NightEssence + number;
+    document.getElementById("NightEssence").innerHTML = NightEssence;
+}; 
+
+var Trolls = 0;
+    function gainTroll(number){
+    Trolls = Trolls + number;
+    document.getElementById("Trolls").innerHTML = Trolls;
+}; 
+
+//Trolls-------------------------------------------------------------------
+
+
