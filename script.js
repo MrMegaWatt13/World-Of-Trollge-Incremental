1cdc6fbe5b0c9c23ac40dee2021c111081d3235c
MrMegaWatt13 <112874353+MrMegaWatt13@users.noreply.github.com>
2022-09-26 14:23:18 +0800

    Alpha 1.0.0

--- script.js
+++ script.js
@@ -2,41 +2,49 @@
 //O-Oil B-Blood M-Mysterious S-Sorrow NE-Night Essence
 
 var GainCups = 0;
+
     function gainGain(number){
     GainCups = GainCups + number;
     document.getElementById("GainCups").innerHTML = GainCups;
 };
+
 var OCups = 0;
+
     function gainOCup(number){
     OCups = OCups + number;
     document.getElementById("OCups").innerHTML = OCups;
 }; 
 
 var BCups = 0;
+
    function gainBCup(number){
     BCups = BCups + number;
     document.getElementById("BCups").innerHTML = BCups;
 }; 
  
 var MCups = 0;
+
     function gainMCup(number){
     MCups = MCups + number;
     document.getElementById("MCups").innerHTML = MCups;
 }; 
  
 var SCups = 0;
+
     function gainSCup(number){
     SCups = SCups + number;
     document.getElementById("SCups").innerHTML = SCups;
 }; 
 
 var NE = 0;
+
     function gainNE(number){
     NE = NE + number;
     document.getElementById("NE").innerHTML = NE;
 }; 
 
 var Trolls = 0;
+
     function gainTrolls(number){
     Trolls = Trolls + number;
     document.getElementById("Trolls").innerHTML = Trolls;
@@ -179,6 +187,6 @@ function getTroll(){
 window.setInterval(function(){
  
     gainTrolls(Troll);  
-    gainTrolls(100),(TestTroll); 
+    gainTrolls(TestTroll);
     
 }, 1000);
